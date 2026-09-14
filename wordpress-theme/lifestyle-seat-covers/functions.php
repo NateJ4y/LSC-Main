0c6a048badd6d19e9d463e503db88579349a8889
NateJ4y <99109049+NateJ4y@users.noreply.github.com>
2026-09-14 22:41:59 +0200

    Add WordPress theme bootstrap and Elementor integration

---
+++ wordpress-theme/lifestyle-seat-covers/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Lifestyle Seat Covers WordPress theme bootstrap.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/enqueue.php';
+require_once get_template_directory() . '/inc/elementor.php';
+require_once get_template_directory() . '/inc/template-functions.php';
