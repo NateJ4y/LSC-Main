9d233b5e4c9ef7f7db14d4f6e94272821b8062e4
NateJ4y <99109049+NateJ4y@users.noreply.github.com>
2026-09-14 22:42:20 +0200

    Add Elementor theme compatibility and editable regions

---
+++ wordpress-theme/lifestyle-seat-covers/inc/elementor.php
@@ -0,0 +1,26 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function lsc_elementor_theme_support() {
+	add_theme_support( 'elementor' );
+}
+add_action( 'after_setup_theme', 'lsc_elementor_theme_support' );
+
+function lsc_register_elementor_locations( $manager ) {
+	$manager->register_all_core_location();
+}
+add_action( 'elementor/theme/register_locations', 'lsc_register_elementor_locations' );
+
+function lsc_elementor_content_wrapper_start() {
+	if ( class_exists( '\Elementor\Plugin' ) && \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+		echo '<main id="primary" class="lsc-editor-content lsc-elementor-editing">';
+	}
+}
+
+function lsc_elementor_content_wrapper_end() {
+	if ( class_exists( '\Elementor\Plugin' ) && \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+		echo '</main>';
+	}
+}
